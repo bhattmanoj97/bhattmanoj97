ee1ed496b4bdd12f1ba0e918c40b736f2f7a3b3e
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-30 13:30:23 +0530

    Update index.php

--- shopify-generating-api-token-guide-master/index.php
+++ shopify-generating-api-token-guide-master/index.php
@@ -1,10 +1,18 @@
 <?php
-$url = "https://08b22714d544265d95b679ec26bdbcaf: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@shopbhatt.myshopify.com/admin/api/2021-07/orders.json";
-$client = new GuzzleHttp/Client();
-$headers = [
-  'X-Shopify-Access-Token' => $oauthToken,
-  'Content-Type' => 'application/json',
-];
-$response = $client->get($url, ['verify' => false, 'headers' => $headers]);
-return (string) $response->getBody();
+
+$url = "https://08b22714d544265d95b679ec26bdbcaf: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@shopbhatt.myshopify.com/admin/api/2021-07/products.json";
+
+$shopcurl = curl_init();
+curl_setopt($shopcurl, CURLOPT_URL, $url);
+curl_setopt($shopcurl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($shopcurl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($shopcurl, CURLOPT_VERBOSE, 0);
+curl_setopt($shopcurl, CURLOPT_HEADER, 1);
+curl_setopt($shopcurl, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($shopcurl, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec ($shopcurl);
+curl_close ($shopcurl);
+echo "<pre>";
+print_r($response);
+
 ?>
