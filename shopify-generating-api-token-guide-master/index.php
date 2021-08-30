e58c914998980cf64f6f30fc5b32c3b2b33579ec
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-30 13:27:12 +0530

    Update index.php

--- shopify-generating-api-token-guide-master/index.php
+++ shopify-generating-api-token-guide-master/index.php
@@ -1,20 +1,10 @@
 <?php
-$client = new GuzzleHttp\Client();
-$res = $client->request('GET', 'https://08b22714d544265d95b679ec26bdbcaf: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@shopbhatt.myshopify.com/admin/api/2021-07/orders.json
-', [
-    'auth' => ['user', 'pass']
-]);
-echo $res->getStatusCode();
-// "200"
-echo $res->getHeader('content-type')[0];
-// 'application/json; charset=utf8'
-echo $res->getBody();
-// {"type":"User"...'
-
-// Send an asynchronous request.
-$request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
-$promise = $client->sendAsync($request)->then(function ($response) {
-    echo 'I completed! ' . $response->getBody();
-});
-$promise->wait();
+$url = "https://08b22714d544265d95b679ec26bdbcaf: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@shopbhatt.myshopify.com/admin/api/2021-07/orders.json";
+$client = new GuzzleHttp/Client();
+$headers = [
+  'X-Shopify-Access-Token' => $oauthToken,
+  'Content-Type' => 'application/json',
+];
+$response = $client->get($url, ['verify' => false, 'headers' => $headers]);
+return (string) $response->getBody();
 ?>
