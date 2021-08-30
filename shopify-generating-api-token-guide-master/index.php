5ea379e1ecf3334a6f04b9227c07ee151d9dcfe3
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-30 13:40:37 +0530

    Update index.php

--- shopify-generating-api-token-guide-master/index.php
+++ shopify-generating-api-token-guide-master/index.php
@@ -1,7 +1,19 @@
 <?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $your_url);
+curl_setopt($ch, CURLOPT_FAILONERROR, true); // Required for HTTP error codes to be reported via our call to curl_error($ch)
+//...
+curl_exec($ch);
+if (curl_errno($ch)) {
+    $error_msg = curl_error($ch);
+}
+curl_close($ch);
+
+if (isset($error_msg)) {
+    // TODO - Handle cURL error accordingly
+}
 
 $url = "https://08b22714d544265d95b679ec26bdbcaf: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@shopbhatt.myshopify.com/admin/api/2021-07/products.json";
-
 $shopcurl = curl_init();
 curl_setopt($shopcurl, CURLOPT_URL, $url);
 curl_setopt($shopcurl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
