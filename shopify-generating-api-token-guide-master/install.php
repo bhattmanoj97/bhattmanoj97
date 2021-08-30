c3f80482993e41150dcbd592548d0bf154670b47
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-30 13:34:37 +0530

    Update install.php

--- shopify-generating-api-token-guide-master/install.php
+++ shopify-generating-api-token-guide-master/install.php
@@ -4,7 +4,7 @@
 $shop = $_GET['shop'];
 $api_key = "08b22714d544265d95b679ec26bdbcaf";
 $scopes = "read_orders,write_products";
-$redirect_uri = "http://localhost/generate_token.php";
+$redirect_uri = "https://manojfirst1.herokuapp.com;
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
